c4e8d3fb4e231eb07385ce3ef1b1860ba6c00d5b
lsacy <68734727+Lsacy@users.noreply.github.com>
2023-02-11 18:43:59 +0100

    api hide

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,41 +2,44 @@ import openai
 import streamlit as st
 from streamlit_chat import message
 from transformers import pipeline
+
+
 summarizer = pipeline("summarization", model="philschmid/bart-large-cnn-samsum")
 sentiment_task = pipeline("sentiment-analysis", model='cardiffnlp/twitter-roberta-base-sentiment-latest', tokenizer='cardiffnlp/twitter-roberta-base-sentiment-latest')
 openai.api_key = st.secrets["openai_api_key"]
 
-from math import log
-
 completion = openai.Completion()
-
 
 start_prompt = '[Instruction] Act as a friendly, compasionate, insightful, and empathetic AI therapist named Joy. Joy listens and offers advices. End the conversation when the patient wishes to.'
 start_message = 'I am Joy, your AI therapist. How are you feeling today?'
 start_sequence = "\nJoy:"
 restart_sequence = "\n\nPatient:"
-  
+
 def ask(question: str, chat_log: str, model='text-davinci-003', temp=0.9) -> (str, str):
+    ''' funtion takes a input string and the preview chat_log, returns the model's repsonse/answer and the dialog as log
+    by using the preview chat_log, gold fish memory effect can be prevented,
+    the chat_log is used to summarize and analyze the sentiment of the user's input '''
+    
+    prompt = f'{chat_log}{restart_sequence} {question}{start_sequence}'
+    response = completion.create(
+        prompt = prompt,
+        model = model,
+        stop = ["Patient:",'Joy:'],
+        temperature = temp, #the higher the more creative
+        frequency_penalty = 0.9, #prevents word repetition, larger -> higher penalty
+        presence_penalty = 1, #prevents topic repetition, larger -> higher penalty
+        top_p =1, 
+        best_of=1,
+        max_tokens=170,
+        )
 
-  prompt = f'{chat_log}{restart_sequence} {question}{start_sequence}'
+    answer = response.choices[0].text.strip()
+    log = f'{restart_sequence}{question}{start_sequence}{answer}'
+    return str(answer), str(log)
 
-  response = completion.create(
-      prompt = prompt,
-      model = model,
-      stop = ["Patient:",'Joy:'],
-      temperature = temp, #the higher the more creative
-      frequency_penalty = 0.9, #prevents word repetition, larger -> higher penalty
-      presence_penalty = 1, #prevents topic repetition, larger -> higher penalty
-      top_p =1, 
-      best_of=1,
-      max_tokens=170
-  )
-  
-  answer = response.choices[0].text.strip()
-  log = f'{restart_sequence}{question}{start_sequence}{answer}'
-  return str(answer), str(log)
+def clean_chat_log(chat_log: list) -> str:
+    ''' cleans the chat log to be used for summarization and sentiment analysis '''
 
-def clean_chat_log(chat_log):
     chat_log = ' '.join(chat_log)
     # find the first /n
     first_newline = chat_log.find('\n')
@@ -45,24 +48,22 @@ def clean_chat_log(chat_log):
     chat_log = chat_log.replace('\n', ' ')
     return chat_log
 
-def summarize(chat_log):
+def summarize(chat_log: list) -> str:
+    ''' returns a summary of the chat log '''
+
     chat_log = clean_chat_log(chat_log)
-    summary = summarizer(chat_log, max_length=150, do_sample=False)[0]['summary_text']
+    summary = summarizer(chat_log, do_sample=False)[0]['summary_text']
     return summary
 
-def analyze_sentiment(chat_log):
-    # split chat_log into smaller chunks
+def analyze_sentiment(user_input: list) -> str:
+    ''' returns user sentiment based on the users input'''
 
-    # analyze each chunk
-
-    # return the average sentiment
-
-
-    chat_log = clean_chat_log(chat_log)
-    sentiment = sentiment_task(chat_log)
+    user_input = clean_chat_log(user_input)
+    summary = summarizer(user_input, do_sample=False)[0]['summary_text']
+    sentiment = sentiment_task(summary)
     return sentiment
 
-def remove_backslashN(chat_log: list) -> list:
+def remove_backslash(chat_log: list) -> list:
     chat_log = [i.replace('\n', ' ') for i in chat_log]
     return chat_log
 
@@ -93,13 +94,13 @@ def main():
             summary = summarizer(chat_log, max_length=100, min_length=30, do_sample=False)
             st.write(summary)
             user_sentiment = st.session_state['past']
-            user_sentiment = remove_backslashN(user_sentiment)
-            st.write(sentiment_task(user_sentiment))
+            user_sentiment = remove_backslash(user_sentiment)
+            st.write(analyze_sentiment(user_sentiment))
             st.session_state['generated'] = [start_message]
             st.session_state['past'] = []
             st.session_state['chat_log'] = [start_prompt+start_sequence+start_message]
             st.session_state['summary'] = []
-
+        
     user_input=st.text_input("You:",key='input')
 
     if user_input:
@@ -107,9 +108,6 @@ def main():
         st.session_state['chat_log'].append(chat_log)
         st.session_state['past'].append(user_input)
         st.session_state['generated'].append(output)
-        print(model)
-        print(temp)
-        print(st.session_state['chat_log'])
     if st.session_state['generated']:
         for i in range(len(st.session_state['generated'])-1, -1, -1):
             if i < len(st.session_state['past']):
@@ -120,11 +118,7 @@ def main():
 
 if __name__ == "__main__":
     main()
-    # save the user's input and the model's output to the database and analyze the user's input and the model's output
 
-    # if len(st.seesion_state['generated'])  :
-        # save the user's input and the model's output to the database
-        # analyze the user's input and the model's output
         
 
 
